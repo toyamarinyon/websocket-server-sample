1f2c232f6985d3392a44e11da31df565a7d5ddd7
toyama satoshi <toyamarinyon@gmail.com>
2021-07-02 10:54:23 +0900

    update

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-var WebSocketServer = require('ws').Server;
-const port = process.env.PORT || 3000;
-var wss = new WebSocketServer({ port });
+'use strict';
 
-console.log(`listening port :${port}`);
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+
+// console.log(`listening port :${PORT}`);
 
 wss.on('connection', function connection(ws) {
     console.log('connection');
