97aca6c0614d57153611478f581fa26846796234
toyama satoshi <toyamarinyon@gmail.com>
2021-07-02 11:51:41 +0900

    bloadcast message

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ wss.on('connection', function connection(ws) {
 
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
-
-        ws.send(message);
+        wss.clients.forEach((client) => {
+            client.send(message);
+        });
     });
 
     ws.on('close', function() {
